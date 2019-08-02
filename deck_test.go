353d4d2fcd20344512d1e38b9fe3aaca65436c4d
Mike Upton <uptonm@wit.edu>
2019-08-01 21:01:56 -0400

    Finished Testing in go

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,27 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck of length 52, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Clubs" {
+		t.Errorf("Expected deck[i] to be 'Ace of Spades', but got '%v'", d[0])
+	}
+
+	if d[len(d)-1] != "King of Spades" {
+		t.Errorf("Expected deck[i] to be 'King of Spades', but got '%v'", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck of length 52, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
